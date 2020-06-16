30e419c356b68a0a0c27f20972687453b39e316a
Agustin Vignolo <agus88lp@gmail.com>
2020-06-15 21:51:18 -0300

    Updates

---
+++ precache-manifest.d97e5681cc9dde926b7fa7dd60740cc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9d2348acff49c76b013dd0496ca8d8f",
+    "url": "/react-noticias/index.html"
+  },
+  {
+    "revision": "cbc65fcdf26815c29431",
+    "url": "/react-noticias/static/css/main.47a9b450.chunk.css"
+  },
+  {
+    "revision": "d8020ad95fb76c38b7eb",
+    "url": "/react-noticias/static/js/2.1346f748.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-noticias/static/js/2.1346f748.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cbc65fcdf26815c29431",
+    "url": "/react-noticias/static/js/main.7a645749.chunk.js"
+  },
+  {
+    "revision": "6820129a3b45a53a5346",
+    "url": "/react-noticias/static/js/runtime-main.25e30c89.js"
+  }
+]);
