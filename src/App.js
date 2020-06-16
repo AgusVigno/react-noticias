b4184103be9187ee041b47c3fae2fee6fc9ee10d
Agustin Vignolo <agus88lp@gmail.com>
2020-06-15 21:46:38 -0300

    vinculado a GH-Pages

--- src/App.js
+++ src/App.js
@@ -1,26 +1,45 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Fragment, useState, useEffect} from 'react';
+import Header from './components/Header';
+import Formulario from './components/Formulario';
+import ListadoNoticias from './components/ListadoNoticias';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	//definir la categoria y las noticias
+	const [categoria, guardarCategoria] = useState('');
+	const [noticias, guardarNoticias] = useState([]);
+
+	useEffect(() => {
+		//consultar en la API
+		const consultarAPI = async () => {
+			const apiKey = 'c00c62a1ac43428cb85addb846c1eef3'
+			const url = `https://newsapi.org/v2/top-headlines?country=ar&category=${categoria}&apiKey=${apiKey}`;
+
+			const resultado = await fetch(url);
+			const noticias = await resultado.json();
+
+			guardarNoticias(noticias.articles);
+		}
+		consultarAPI();
+	}, [categoria]);
+
+	return (
+		<Fragment>
+			<Header
+				titulo='Buscador de Noticias'
+			/>
+
+			<div className="container white">
+				<Formulario 
+					guardarCategoria={guardarCategoria}
+				/>
+
+				<ListadoNoticias
+					noticias={noticias}
+				/>
+			</div>
+		</Fragment>
+);
 }
 
 export default App;
